54ed45b40271d577528f6f87686ac1cacb0ee28d
xinjiahui <xinjiahuiqh@gmail.com>
2018-04-11 11:35:48 +0800

    changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,4 @@ Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
