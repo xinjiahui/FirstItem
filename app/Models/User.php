a0529bbe3a18ac58c1cbc8825e4325b50fde0d08
xinjiahui <xinjiahuiqh@gmail.com>
2018-04-13 16:02:42 +0800

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,12 @@ class User extends Authenticatable
         return "http://123.56.218.155/0068yAEFly8fl8kqzmb6nj30yi0yiq50.jpg/$hash?s=$size";
         #return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+   public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'xinjiahuiqh@gmail.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
